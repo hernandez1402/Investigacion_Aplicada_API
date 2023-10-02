a8f39f5fa7afa44953d24ed09cb680028fbd605e
kelvigg11 <kelvinalfaro@outlook.es>
2023-10-02 16:17:22 -0600

    Agregando key/value Prerequirements:[array] a todas las materias que necesiten prerequisitos antes de cursar la materia elegida

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const tecnico = [
     {id: 3, name: 'Programación de Algoritmos', credits: 4, enrolled: true},
     {id: 4, name: 'Redes de Comunicación', credits: 4, enrolled: true},
     {id: 5, name: 'Análisis y Diseño de Sistemas y Base de Datos', credits: 4, enrolled: true},
-    {id: 6, name: 'Desarrollo de Aplic. Web con Soft. Interpret. en el Cliente', credits: 4, enrolled: true},
-    {id: 7, name: 'Desarrollo de Aplicaciones con Software Propietario', credits: 4, enrolled: true},
-    {id: 8, name: 'Programación Orientada a Objetos', credits: 4, enrolled: true},
-    {id: 9, name: 'Administración de Servicios en la Nube', credits: 4, enrolled: true},
-    {id: 10, name: 'Diseño y Programación de Software Multiplataforma', credits: 4, enrolled: true},
+    {id: 6, name: 'Desarrollo de Aplic. Web con Soft. Interpret. en el Cliente', prerequirements:[2],credits: 4, enrolled: true},
+    {id: 7, name: 'Desarrollo de Aplicaciones con Software Propietario', prerequirements:[3],credits: 4, enrolled: true},
+    {id: 8, name: 'Programación Orientada a Objetos', prerequirements:[3], credits: 4, enrolled: true},
+    {id: 9, name: 'Administración de Servicios en la Nube', prerequirements:[3,4], credits: 4, enrolled: true},
+    {id: 10, name: 'Diseño y Programación de Software Multiplataforma',prerequirements:[5,6] ,credits: 4, enrolled: true},
 ]
 
 const ingenieria = [
@@ -22,11 +22,11 @@ const ingenieria = [
     {id: 3, name: 'Quimica General', credits: 4, enrolled: true},
     {id: 4, name: 'Sistemas Operativos', credits: 4, enrolled: true},
     {id: 5, name: 'Ingenieria de Software', credits: 4, enrolled: true},
-    {id: 6, name: 'Lenguajes Interpretados en el Cliente', credits: 4, enrolled: true},
-    {id: 7, name: 'Lenguajes Interpretados en el Servidor', credits: 4, enrolled: true},
-    {id: 8, name: 'Programación Estructurada', credits: 4, enrolled: true},
-    {id: 9, name: 'Estadistica Aplicada', credits: 4, enrolled: true},
-    {id: 10, name: 'programacion Orientada a Objetos', credits: 4, enrolled: true},
+    {id: 6, name: 'Lenguajes Interpretados en el Cliente',prerequirements:[4,5], credits: 4, enrolled: true},
+    {id: 7, name: 'Lenguajes Interpretados en el Servidor', prerequirements:[4,5],credits: 4, enrolled: true},
+    {id: 8, name: 'Programación Estructurada', prerequirements:[4,5],credits: 4, enrolled: true},
+    {id: 9, name: 'Estadistica Aplicada', prerequirements:[1,2],credits: 4, enrolled: true},
+    {id: 10, name: 'programacion Orientada a Objetos',prerequirements:[4,5], credits: 4, enrolled: true},
 ]
 
 app.get('/', (req, res) => {
@@ -36,10 +36,8 @@ app.get('/', (req, res) => {
 app.get('/api/tecnico', (req, res) =>{
     res.send(tecnico);
 })
-
 app.get('/api/ingenieria', (req, res) =>{
     res.send(ingenieria);
 })
-
 const port = process.env.port || 80;
 app.listen(port, () => console.log(`Escuchando en puerto ${port}...`));
